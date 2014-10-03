49d376d59525b1b04bb0dbfde85c60f76bbe4eda
Eugen Grouk <evgeniy.gruk@oxagile.com>
2014-10-03 12:36:07 +0300

    User model added, Role model added

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -1,4 +1,4 @@
-CREATE TABLE tbl_user (
+CREATE TABLE IF NOT EXISTS tbl_user (
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(128) NOT NULL,
     password VARCHAR(128) NOT NULL,
@@ -9,17 +9,17 @@ CREATE TABLE tbl_user (
     fb_id VARCHAR(128) NULL
 );
 
-INSERT INTO tbl_user (username, password, email, role_id, avatar, nickname, fb_id) VALUES ('qweasd', '123123', 'evgeniy.gruk@oxagile.com', 1, '', '', '', '');
+INSERT INTO tbl_user (username, password, email, role_id, avatar, nickname, fb_id) VALUES ('qweasd', '123123', 'evgeniy.gruk@oxagile.com', 1, '', '', '');
 INSERT INTO tbl_user (username, password, email, role_id, avatar, nickname, fb_id) VALUES ('test1', 'qweqwe', 'test1@example.com', '', '', '', '');
 INSERT INTO tbl_user (username, password, email, role_id, avatar, nickname, fb_id) VALUES ('test2', 'qweqwe', 'test2@example.com', '', '', '', '');
 INSERT INTO tbl_user (username, password, email, role_id, avatar, nickname, fb_id) VALUES ('test3', 'qweqwe', 'test3@example.com', '', '', '', '');
 INSERT INTO tbl_user (username, password, email, role_id, avatar, nickname, fb_id) VALUES ('test4', 'qweqwe', 'test4@example.com', '', '', '', '');
 INSERT INTO tbl_user (username, password, email, role_id, avatar, nickname, fb_id) VALUES ('test5', 'qweqwe', 'test5@example.com', '', '', '', '');
 
-CREATE TABLE tbl_role (
+CREATE TABLE IF NOT EXISTS tbl_role (
     id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(128) NOT NULL,
-    users_qty INTEGER NOT NULL,
+    users_qty INTEGER NOT NULL
 );
 
 INSERT INTO tbl_role (title, users_qty) VALUES ('Administrator', 1);
---
+++ protected/models/User.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * This is the model class for table "{{user}}".
+ *
+ * The followings are the available columns in table '{{user}}':
+ * @property integer $id
+ * @property string $username
+ * @property string $password
+ * @property string $email
+ * @property integer $role_id
+ * @property string $avatar
+ * @property string $nickname
+ * @property string $fb_id
+ */
+class User extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return '{{user}}';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('username, password, email, role_id, nickname', 'required'),
+			array('role_id', 'numerical', 'integerOnly'=>true),
+			array('username, password, email, nickname, fb_id', 'length', 'max'=>128),
+			array('avatar', 'length', 'max'=>256),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, username, password, email, role_id, avatar, nickname, fb_id', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'username' => 'Username',
+			'password' => 'Password',
+			'email' => 'Email',
+			'role_id' => 'Role',
+			'avatar' => 'Avatar',
+			'nickname' => 'Nickname',
+			'fb_id' => 'Fb',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('username',$this->username,true);
+		$criteria->compare('password',$this->password,true);
+		$criteria->compare('email',$this->email,true);
+		$criteria->compare('role_id',$this->role_id);
+		$criteria->compare('avatar',$this->avatar,true);
+		$criteria->compare('nickname',$this->nickname,true);
+		$criteria->compare('fb_id',$this->fb_id,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return User the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
